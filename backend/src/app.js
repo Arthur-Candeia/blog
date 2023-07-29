3aefe469b5726ab9965695b1ad90f788fbd5ff1d
Arthur-Candeia <arthurheher@hotmail.com>
2023-07-29 11:39:01 -0300

    adicao do cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const router = require('./router')
-//configurar cors
+const cors = require('cors')
 require('../db/db.js')
 
 app.use(express.json())
+
+app.use((request, response, next) => {
+  response.header("Access-Control-Allow-Origin", '*');
+  response.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  next();
+});
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
+
 app.use('/', router)
 
 const PORT = process.env.PORT || 8080
