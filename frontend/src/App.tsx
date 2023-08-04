2c0272deec00f8a02ebaf3d1db5d810e37f5170d
Arthur-Candeia <arthurheher@hotmail.com>
2023-08-04 13:41:50 -0300

    estrutura de carregamento da pagina

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import Shield from "./components/Shield/Shield"
+import './styles/style.scss';
+import './styles/media-queries.scss';
+
+function App() {
+
+  return (
+    <>
+      <Shield />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Shield/Shield.tsx
@@ -0,0 +1,14 @@
+import './Shield.scss';
+
+export default function Shield() {
+  return (
+    <div id="shield">
+      <header className="header background"></header>
+      <aside className="infos background"></aside>
+      <div className="post1 background"></div>
+      <div className="post2 background"></div>
+      <div className="post3 background"></div>
+      <aside className="links background"></aside>
+    </div>
+  )
+}
