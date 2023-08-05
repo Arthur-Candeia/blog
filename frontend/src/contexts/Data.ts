fc1c796753d9be5a9c65d2350a2feac44110f23e
Arthur-Candeia <arthurheher@hotmail.com>
2023-08-04 21:28:21 -0300

    tipagem do context

--- frontend/src/contexts/Data.ts
+++ frontend/src/contexts/Data.ts
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 import DataTypes from '../interface/DataTypes';
 
-const DataContext = createContext<DataTypes>({posts: [], links: []})
+const DataContext = createContext<DataTypes>({posts: [{title: '', content: '', src: '', date: '', likes: 2}], links: []})
 
 export default DataContext;
