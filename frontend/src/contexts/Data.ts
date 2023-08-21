2b30b86de6069657afd75155a33c952a7ebb900e
Arthur-Candeia <arthurheher@hotmail.com>
2023-08-21 12:05:37 -0300

    fix na tipagem do context

--- frontend/src/contexts/Data.ts
+++ frontend/src/contexts/Data.ts
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 import DataTypes from '../interfaces/DataTypes';
 
-const DataContext = createContext<DataTypes>({posts: [{title: '', content: '', src: '', date: '', likes: 2}], links: [{title: '', url: ''}]})
+const DataContext = createContext<DataTypes>({posts: [{title: '', content: '', src: '', date: '', likes: 2, _id: ''}], links: [{title: '', url: ''}]})
 
 export default DataContext;
